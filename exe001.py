81e7411f8e725b25b37caaf41b80b316bb20316f
Joziane Oliveira <jozioliveira@edu.uniube.br>
2024-12-30 06:30:49 -0300

    Update exe001.py

--- exe001.py
+++ exe001.py
@@ -2,3 +2,7 @@ print('Olá, Mundo!')
 
 nome=input('Qual é o seu nome? ')
 print('Prazer em te conhecer {}!'.format(nome))
+n1=int(input('Digite um número: '))
+n2=int(input('Digite outro número: '))
+s=int(n1)+int(n2)
+print('A soma entre {} e {} é igual a {}'.format(n1, n2, s))    
